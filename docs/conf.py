67526a9346aae41389461bf8f51995898c10141b
Jeremy Arr <jeremyarr@gmail.com>
2017-07-01 18:39:49 +1000

    rtd theming

--- docs/conf.py
+++ docs/conf.py
@@ -91,8 +91,9 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {"github_fork":"jeremyarr/jenkins_badges",
-                      "index_logo":"logo_full.png"}
+html_theme_options = {}
+# html_theme_options = {"github_fork":"jeremyarr/jenkins_badges",
+#                       "index_logo":"logo_full.png"}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
